ac7528340f8c4657c7ab7e206deb0b18d89a7925
Denis Zenkov <id90814@gmail.com>
2024-09-26 12:02:22 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Anton' : 1999, 'Alex' : 1997, 'Sasha' : 2001}
+print(my_dict)
+print(my_dict.get('Alex'))
+print(my_dict.get('Max'))
+my_dict.update({'Denis' : 1995, 'Masha' : 2003})
+print(my_dict)
+del my_dict['Anton']
+print(my_dict)
+
+my_set = {1, 2 , 1, 1, 3, 4 , 5}
+print(my_set)
+my_set.add(6)
+my_set.add(7)
+my_set.discard(1)
+print(my_set)
